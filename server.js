f73c646add9f241835267dc2f37191b200bce1d4
Michael Robards <mikerobards@icloud.com>
2022-08-31 17:02:53 -0400

    add image

--- server.js
+++ server.js
@@ -52,6 +52,35 @@ app.post('/register', (req, res) => {
     res.json(database.users[database.users.length - 1]);
 })
 
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    })
+    if (!found) {
+        res.status(404).json('user not found!');
+    }
+})
+
+app.post('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    })
+    if (!found) {
+        res.status(404).json('user not found!');
+    }
+})
+
 app.listen(3000, () => {
     console.log('app is running on port 3000!');
 });
@@ -59,10 +88,7 @@ app.listen(3000, () => {
 
 /*
 
-/ --> res = this is working
-/signin --> POST = success/fail
-/register --> POST = user
-/profile/:userId --> GET = user
 /image --> PUT --> user
+NEED TO CORRECT TO PUT
 
 */
