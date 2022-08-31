06b85e1997862c91ecab783f1bfe2e80df44d1fb
Michael Robards <mikerobards@icloud.com>
2022-08-31 16:31:57 -0400

    add signin

--- server.js
+++ server.js
@@ -2,8 +2,41 @@ const express = require('express');
 
 const app = express();
 
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'Mike',
+            email: 'mike@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+
+    ]
+}
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send('this is working!!!')
+    res.send('this is working!!!');
+});
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+        res.json('Success!');
+    } else {
+        res.status(400).json('error loggin in!');
+    }
 })
 
 app.listen(3000, () => {
